46c29dcb4c34f9aa2e482a3b87753862331cb31d
ander <andersonbarreto2001@gmail.com>
2025-01-13 20:37:13 +0100

    wip serialize class Task

--- tasks/models.py
+++ tasks/models.py
@@ -1,32 +1,65 @@
 from datetime import datetime
 from enum import Enum
 
+from json import dumps
 
-class __Status(Enum):
-    PENDING = 1
-    IN_POGRESS = 2
-    COMPLETED = 3
+
+class Status(Enum):
+    PENDING = "Pending"
+    IN_PROGRESS = "In progress"
+    COMPLETED = "Completed"
 
 
 class Task:
 
-    def __init__(self, title: str, description: str, status: __Status, completed_at) -> None:
-        """
-        status: can be - pending, in pogress, completed
-        """
-        self.id = "?"
+    __current_id = 0
+
+    def __init__(
+        self,
+        title: str,
+        description: str,
+        status: Status = Status.PENDING,
+        completed_at: datetime = None,
+    ) -> None:
+        self.valid_status(status)
+        Task.__current_id += 1
+        self.id = Task.__current_id
         self.title = title
         self.description = description
         self.status = status
         self.created_at = datetime.now()
         self.completed_at = completed_at
 
-    def mark_completed(id: int, data) -> None:
-        # to do search in data all the tasks with the id
-        # and verify de field status if is 1 then is completed
-        # else is incompleted
+    def mark_completed(self) -> None:
+        self.status = Status.COMPLETED
+        self.completed_at = datetime.now()
 
-        pass
+    @classmethod
+    def reset_id(cls):
+        cls.__current_id = 0
+
+    @classmethod
+    def valid_status(self, status):
+        if not isinstance(status, Status):
+            raise ValueError(f"Invalid Status: {status}")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "status": self.status.value,
+            "created_at": self.created_at.isoformat(),
+            "completed_at": (
+                self.completed_at.isoformat() if self.completed_at else None
+            ),
+        }
 
 
-print(__Status.COMPLETED)
+task = Task("Learn Python", "Improve python skill")
+
+task_dict = task.to_dict()
+
+task_json = dumps(task_dict, indent=4)
+
+print(task_json)
