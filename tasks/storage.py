6f2e14043c112512db1f288af9fe3a5871603b5c
ander <andersonbarreto2001@gmail.com>
2025-01-15 20:54:09 +0100

    fix: load data in json and adding func for load the data for the output

--- tasks/storage.py
+++ tasks/storage.py
@@ -1,13 +1,22 @@
 from .models import Task
 import json
 
+
 class Storage:
     __PATH_JSON = "./data/tasks.json"
 
     def save_tasks(self, tasks: list[Task]):
-        with open(self.__PATH_JSON, 'w') as file:
+        with open(self.__PATH_JSON, "w") as file:
+            data = []
             for task in tasks:
-                file.write(json.dumps(task.to_dict(), indent=4))
+                data.append(task.to_dict())
+            json.dump(data, file, indent=4)
 
     def load_tasks(self):
+        with open(self.__PATH_JSON, "r") as file:
+            data = json.load(file)
+            return data
+
+    # to do
+    def backup_data(self):
         pass
