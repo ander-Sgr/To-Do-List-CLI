9e65baadb06dc491bc5cf6101383f16e0f768b5c
ander <andersonbarreto2001@gmail.com>
2025-02-18 00:56:20 +0100

    fix some stuffs

--- tasks/models.py
+++ tasks/models.py
@@ -1,8 +1,6 @@
 from datetime import datetime
 from enum import Enum
 
-from json import dumps
-
 
 class Status(Enum):
     PENDING = "Pending"
@@ -18,21 +16,28 @@ class Task:
         self,
         title: str,
         description: str,
+        id: int = None,
         status: Status = Status.PENDING,
         completed_at: datetime = None,
+        created_at: datetime = datetime.now()
     ) -> None:
         self.valid_status(status)
-        Task.__current_id += 1
-        self.id = Task.__current_id
         self.title = title
         self.description = description
         self.status = status
-        self.created_at = datetime.now()
+        self.created_at = created_at
         self.completed_at = completed_at
+        self.id = self.set_id(id)
 
     def mark_completed(self) -> None:
         self.status = Status.COMPLETED
         self.completed_at = datetime.now()
+
+    def set_id(self, id):
+        if id is None:
+            Task.__current_id += 1
+            id = Task.__current_id
+        return id
 
     @classmethod
     def reset_id(cls):
@@ -54,3 +59,18 @@ class Task:
                 self.completed_at.isoformat() if self.completed_at else None
             ),
         }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            id=data["id"],
+            title=data["title"],
+            description=data["description"],
+            status=Status(data["status"]),
+            created_at=datetime.fromisoformat(data["created_at"]),
+            completed_at=(
+                datetime.fromisoformat(data["completed_at"])
+                if data["completed_at"]
+                else None
+            ),
+        )
