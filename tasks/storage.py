66f1c9025a0ff9e7fbd67b8df34d1da2ae961c53
ander <andersonbarreto2001@gmail.com>
2025-02-18 00:57:43 +0100

    fix storage

--- tasks/storage.py
+++ tasks/storage.py
@@ -5,17 +5,20 @@ import json
 class Storage:
     __PATH_JSON = "./data/tasks.json"
 
-    def save_tasks(self, tasks: list[Task]):
-        with open(self.__PATH_JSON, "w") as file:
-            data = []
-            for task in tasks:
-                data.append(task.to_dict())
-            json.dump(data, file, indent=4)
-
     def load_tasks(self):
-        with open(self.__PATH_JSON, "r") as file:
-            data = json.load(file)
-            return data
+        try:
+            with open(self.__PATH_JSON, "r") as file:
+                return json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            return []
+
+    def save_all_tasks(self, tasks: list):
+        """Sobrescribe el archivo JSON con todas las tareas actualizadas."""
+        try:
+            with open(self.__PATH_JSON, "w") as file:
+                json.dump(tasks, file, indent=4)
+        except Exception as e:
+            print(f"Error while saving tasks: {e}")
 
     # to do
     def backup_data(self):
