de01c8d2d5808f7b22a84ab728535dc47d91d5f9
ander <andersonbarreto2001@gmail.com>
2025-02-18 00:57:04 +0100

    implementing crud, to do delete funcctio

--- tasks/manager.py
+++ tasks/manager.py
@@ -1,31 +1,45 @@
 from .models import Task, Status
+from .storage import Storage
 
 
 class TaskManager:
-    def __init__(self, tasks: list[Task] = None) -> None:
-        self.tasks = tasks if tasks is not None else []
+    def __init__(self, storage: Storage) -> None:
+        self.storage = storage
+        self.tasks = self.storage.load_tasks()
 
-    def add_task(self, task: Task):
-        self.tasks.append(task)
+    def convert_task_dict(self, task: Task):
+        if isinstance(task, dict):
+            data = Task.from_dict(task)
+            return data
 
-    def __exist_task(self, id):
+    def __exists_task(self, id) -> Task:
         for task in self.tasks:
-            if task.id == id:
+            if task["id"] == id:
                 return task
         return None
 
-    def get_task(self, task_id: int):
-        task = self.__exist_task(task_id)
-        if task:
-            return task
+    def add_task(self, task: Task):
+        self.tasks.append(task.to_dict())
+        self.storage.save_all_tasks(self.tasks) 
 
-    def edit_task(self, task_id: int, title: str, description: str, status: Status):
-        task = self.__exist_task(task_id)
-        if task:
-            task.title = title
-            task.description = description
-            task.status = status
+    def get_task(self, id):
+        return self.__exists_task(id)
 
+    def modify_task(self, updated_task: Task, id):
+        task = self.__exists_task(id)
+        if task:
+            if isinstance(task, dict):
+                task = Task.from_dict(task)
+                task.title = updated_task.title
+                task.description = updated_task.description
+                task.status = updated_task.status
+                for i, d in enumerate(self.tasks):
+                    if d["id"] == task.id:
+                        print(task.title)
+                        self.tasks[i] = updated_task.to_dict()
+                self.storage.save_all_tasks(self.tasks)
+                        
+                        
     def delete_task(self, task_id: int):
         task = self.__exist_task(task_id)
         if task:
