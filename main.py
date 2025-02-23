42db82dd73f014907a9d64b371d0cea83b48c97f
ander <andersonbarreto2001@gmail.com>
2025-02-23 15:00:11 +0100

    wip cli to do list

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 from tasks import Task, TaskManager, Status, Storage
 from utils import TasksDisplay
+from argparse import ArgumentParser, Namespace
+import sys
 
 storage = Storage()
-task_manager = TaskManager(storage)
+
 display = TasksDisplay(storage)
 
-#task_manager.add_task(Task("Nuevo item", "realizar un insert"))
-#task_manager.add_task(Task("Nuevo item", "realizar un insert"))
-#task_manager.add_task(Task("Nuevo item", "realizar un insert"))
-#task_manager.add_task(Task("Nuevo item", "realizar un insert"))
-t = task_manager.get_task(2)
-new_task = (Task("hola", "descip", t['id']))
-task_new = task_manager.modify_task(new_task, t['id'])
-print("----------------")
-print(t)
-display.show_tasks()
+
+def parser_args(args: list) -> Namespace:
+    parser = ArgumentParser("TO DO list in cli")
+    
+    required = parser.add_argument_group('required arguemnts')
+    required.add_argument('-c', '--create', action='store_true',
+                          help="Add a new task")
+    task_details = parser.add_argument_group('task details')
+    task_details.add_argument('--title', type=str, help="Ttile's task")
+    task_details.add_argument('--descrip', type=str, help="Description's task")
+    
+    return parser.parse_args(args)
+    
+
+def add_new_task(title: str, descrip: str) -> None:
+    task_manager = TaskManager(storage)
+    task_manager.add_task(Task(title, descrip))
+    # display.show_task(task.id)
+    
+
+def main(args: list):
+    parsed_args = parser_args(args)
+    if parsed_args.create:
+       add_new_task(parsed_args.title, parsed_args.descrip)
+    
+if __name__ == "__main__":
+    main(sys.argv[1:])
